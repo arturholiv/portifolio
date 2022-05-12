4c5aa3643391fc07b7ac7a4fc860f9320c397323
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-05-12 16:57:21 -0300

    feat: create App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css';
 import Main from './components/Main';
+import { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+// import 'App.css';
 
 function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
   return (
-    <Main />
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <Main />
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
