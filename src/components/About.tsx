e92ea28a61876a6aa69b642790d2957ced18ee1d
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-06-23 18:33:53 -0300

    update: new position as an IT intern at Lider Aviacao

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
        <p>
        Me chamo Artur Henrique de Oliveira e tenho 21 anos. Sou estudante de desenvolvimento web fullstack na
         <a href="https://www.betrybe.com/" target="_blank" className="link-trybe" rel="noreferrer"> Trybe</a> e
-        também de Análise e desenvolvimento de sistemas na Estácio de Sá.
+        também de Análise e desenvolvimento de sistemas na Estácio de Sá. Atualmente sou estagiário de TI na  <a href="https://www.lideraviacao.com.br/pt-br/pagina-inicial" target="_blank" className="link-trybe" rel="noreferrer"> Líder Aviação </a> atuando com desenvolvimento .Net .
          <br />
          <br />
         Sou apaixonado por tecnologia e por ideias inovadoras o que me leva a ir em busca das melhores 
